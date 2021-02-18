f6a45a86cb2f8151c6d9bb9f3a3a8e44dac20ee9
vedang-jammy <upadhyevedang18@gmail.com>
2021-02-18 20:30:49 +0530

    new examples added

---
+++ Practice programs/primesum.py
@@ -0,0 +1,18 @@
+'''
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+'''
+def isPrime(n):
+    if n < 2: return "Neither prime, nor composite"
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+sum = 0
+for i in range(2, 2000000):
+    if isPrime(i):
+        sum += i
+
+print (sum)
---
+++ Practice programs/pythagorian.py
@@ -0,0 +1,16 @@
+'''
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+'''
+sum1 = 1000
+a=0
+b=0
+c=0
+
+    if a+b+c == sum1:
+            print(a*b*c)
